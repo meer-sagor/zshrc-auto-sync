33cf9e294eb98d1bbc80c4ec4ab6cc103a5e2f8b
Meer Sagor <meersagor.contact@gmail.com>
2023-12-22 17:21:48 +0600

    fix: :bug: remove every time install monitoring app only install ones

--- install_file_monitor.sh
+++ install_file_monitor.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-
-sudo apt-get update
-sudo apt-get install -y inotify-tools
+# Check if inotify-tools is already installed
+if command -v inotifywait &> /dev/null; then
+    echo "inotify-tools is already installed. Skipping installation."
+else
+    # Ask the user if they want to install inotify-tools
+    read -p "inotify-tools is not installed. Do you want to install it? (y/n): " choice
+    if [ "$choice" == "y" ]; then
+        sudo apt-get update
+        sudo apt-get install -y inotify-tools
+        echo "inotify-tools has been installed."
+    else
+        echo "Skipping inotify-tools installation."
+    fi
+fi
--- push_to_github.sh
+++ push_to_github.sh
@@ -2,20 +2,6 @@
 
 source github_config.sh
 
-# FILE_PATH=".zshrc"
-# # shellcheck disable=SC2046
-# cd `dirname "$0"`
-
-# cp "$HOME/$FILE_PATH" .
-
-# git add .
-
-# git commit -m "Sync zsh configs"
-
-# git push origin
-
-# GitHub repository information
-
 # Local file path to copy
 FILE_PATH=".zshrc"
 LOCAL_ZSH_FILE="$HOME/$FILE_PATH"
